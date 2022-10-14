22d6003c4b9b1c3ee68c8be8ac8c92cf8e4c4a7c
Stef-Stefanov <sistefanov@abv.bg>
2022-10-14 13:39:10 +0300

    The project has been born

---
+++ src/main/java/com/example/emag/EmagApplication.java
@@ -0,0 +1,13 @@
+package com.example.emag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmagApplication.class, args);
+	}
+
+}
