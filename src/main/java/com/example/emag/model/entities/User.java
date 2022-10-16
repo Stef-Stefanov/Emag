f3deaa3c4d2dd975fa677086a750c96a7106602f
Stef-Stefanov <sistefanov@abv.bg>
2022-10-16 12:26:35 +0300

    Added user entity

---
+++ src/main/java/com/example/emag/model/entities/User.java
@@ -0,0 +1,38 @@
+package com.example.emag.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String createdAt;
+    @Column
+    private int isSubscribed;
+    @Column
+    private int isAdmin;
+    @Column
+    private String phoneNumber;
+    @Column
+    private String birthDate;
+}
