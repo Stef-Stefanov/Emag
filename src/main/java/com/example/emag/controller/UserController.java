3d39d11a847363c88396bd20889b33e3c5fc5fce
Stef-Stefanov <sistefanov@abv.bg>
2022-10-22 18:44:38 +0300

    UserController Class reworked

--- src/main/java/com/example/emag/controller/UserController.java
+++ src/main/java/com/example/emag/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.example.emag.controller;
 
 import com.example.emag.model.dto.user.LoginDTO;
-import com.example.emag.model.dto.user.UserDTO;
-import com.example.emag.model.repositories.UserRepository;
+import com.example.emag.model.dto.user.RegisterDTO;
+import com.example.emag.model.dto.user.UserWithoutPassDTO;
+import com.example.emag.model.exceptions.BadRequestException;
 import com.example.emag.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class UserController extends AbstractController{
@@ -17,9 +17,28 @@ public class UserController extends AbstractController{
     private UserService userService;
 
     @PostMapping("/users")
-    public UserDTO addUser(@RequestBody LoginDTO dto, HttpServletRequest req){
-        UserDTO result = userService.login(dto);
+    // receive DTO, send to User Service, deserialize in UserService, map to entity, register, send back 200 ok.
+    public RegisterDTO registerUser(@RequestBody RegisterDTO dto, HttpServletRequest req){
+        LoginDTO loginDTO = userService.checkForUser();
+        UserWithoutPassDTO result = userService.login(loginDTO);
         logUser(req, result.getId());
-        return userService.add(dto);
+        return userService.registerUser(dto);
+    }
+
+    @PostMapping("/auth")
+    public UserWithoutPassDTO login(@RequestBody LoginDTO dto, HttpSession s){
+        UserWithoutPassDTO result = userService.login(dto);
+        if(result != null){
+            s.setAttribute("LOGGED", true);
+            s.setAttribute("USER_ID", result.getId());
+            return result;
+        }
+        else{
+            throw new BadRequestException("Wrong Credentials");
+        }
+    }
+    @GetMapping("/users/{uid}")
+    public UserWithoutPassDTO getById(@PathVariable int uid){
+        return userService.getById(uid);
     }
 }
