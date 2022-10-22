f331afc1b5c297ac7540f74ff6dd98f914f8815c
Stef-Stefanov <sistefanov@abv.bg>
2022-10-22 18:41:35 +0300

    Minor Changes and findByMail

--- src/main/java/com/example/emag/model/repositories/UserRepository.java
+++ src/main/java/com/example/emag/model/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.emag.model.repositories;
 import com.example.emag.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
