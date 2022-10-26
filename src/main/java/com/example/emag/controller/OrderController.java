c4dc00ee301479fe4b0ab74b8bd5f512ca776f8f
sdonkov <sotirdonkov@gmail.com>
2022-10-26 12:04:18 +0300

    fix make order url

--- src/main/java/com/example/emag/controller/OrderController.java
+++ src/main/java/com/example/emag/controller/OrderController.java
@@ -1,20 +1,22 @@
 package com.example.emag.controller;
 
 import com.example.emag.model.dto.order.MadeOrderDTO;
-import com.example.emag.model.entities.Order;
 import com.example.emag.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class OrderController extends AbstractController{
     @Autowired
     private OrderService orderService;
 
-    @PostMapping("orders/{uid}")
-    public MadeOrderDTO makeOrder(@PathVariable long uid){
+    @PostMapping("orders")
+    public MadeOrderDTO makeOrder(HttpServletRequest req){
+        checkIfLogged(req);
+        long uid = getLoggedUserId(req);
         return orderService.makeOrder(uid);
     }
 }
