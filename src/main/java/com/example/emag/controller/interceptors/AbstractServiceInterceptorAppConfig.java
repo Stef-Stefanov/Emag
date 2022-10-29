c802a0140d85154415564509ee31a8104d186d76
Stef-Stefanov <sistefanov@abv.bg>
2022-10-29 19:45:58 +0300

    WebMVCConfigurerAdapter class to interface WebMVCConfigurer

--- src/main/java/com/example/emag/controller/interceptors/AbstractServiceInterceptorAppConfig.java
+++ src/main/java/com/example/emag/controller/interceptors/AbstractServiceInterceptorAppConfig.java
@@ -4,10 +4,10 @@ package com.example.emag.controller.interceptors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
-public class AbstractServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
+public class AbstractServiceInterceptorAppConfig implements WebMvcConfigurer {
     @Autowired
     AbstractServiceInterceptor abstractServiceInterceptor;
 
